b09d693298cd963590548d3b6b59225a918047af
Jumbo Daniel <jumbodaniel15@gmail.com>
2023-06-18 20:31:48 +0800

    Added the hero and features section

---
+++ components/landing/hero/constants.ts
@@ -0,0 +1,11 @@
+export const links = ["Resources", "Solutions", "features", "Pricing"];
+export const button = [
+  [
+    "Log in",
+    "text-base font-semibold text-black transition-all duration-200 hover:text-opacity-80",
+  ],
+  [
+    "Try for free",
+    "inline-flex items-center justify-center px-5 py-2.5 text-base font-semibold text-black border-2 border-black hover:bg-black hover:text-white transition-all duration-200 focus:bg-black focus:text-white",
+  ],
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Hero from '@/components/hero/hero'
+import Hero from '@/components/landing/hero/hero'
+import Features from '@/components/landing/features'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={` ${inter.className}`}
     >
 
     <Hero/>
+    <Features/>
     </main>
   )
 }
